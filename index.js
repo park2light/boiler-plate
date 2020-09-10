abeb299de953d769950c0626d3a92c11e1eb628b
Johg <park2light@gmail.com>
2020-09-10 20:27:01 +0900

    test

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 3000
+const {User} = require("./models/User");
+
+const bodyParser = require('body-parser');
+
+// application/x-www-form-urlencoded 된데이터를 분석해서 가져옴
+app.use(bodyParser.urlencoded({extended:true}));  
+// application/json 된데이터를 분석해서 가져옴
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://psh:psh1234@boilerplate.idmdn.mongodb.net/boilerplate?retryWrites=true&w=majority',{
@@ -14,6 +22,26 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.post('/register',(req,res)=>{
+  //회원가입할때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+  
+  // {
+  //   id:"hello",
+  //   password:"123"
+
+  // } 이런식으로 req.body에들어있음 bodyParser가 이걸 가능하게해줌
+
+  
+  const user = new User(req.body)
+
+  user.save((err,userInfo) =>{
+    if(err) return res.json({success:false,err})
+    return res.status(200).json({
+      success:true
+    })
 })
+
+
+})
+
+app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`)})
