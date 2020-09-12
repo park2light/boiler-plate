8e9f85aaaa11f4c16062ccaebfed87e0d61d6d28
Johg <park2light@gmail.com>
2020-09-12 13:51:54 +0900

    tezz

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const app = express()
-const port = 3000
-const {User} = require("./models/User");
-
+const port = 9000
+const cookieParser = require('cookie-parser')
+const { User } = require("./models/User");
+const config = require('./config/key');
 const bodyParser = require('body-parser');
 
 // application/x-www-form-urlencoded 된데이터를 분석해서 가져옴
 app.use(bodyParser.urlencoded({extended:true}));  
 // application/json 된데이터를 분석해서 가져옴
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 const mongoose = require('mongoose')
+
 mongoose.connect('mongodb+srv://psh:psh1234@boilerplate.idmdn.mongodb.net/boilerplate?retryWrites=true&w=majority',{
   useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true,useFindAndModify:false
 }).then(()=>console.log('MongoDB Connected...')) 
@@ -19,7 +21,7 @@ mongoose.connect('mongodb+srv://psh:psh1234@boilerplate.idmdn.mongodb.net/boiler
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!~~~~~변경')
 })
 
 app.post('/register',(req,res)=>{
@@ -34,6 +36,8 @@ app.post('/register',(req,res)=>{
   
   const user = new User(req.body)
 
+
+  
   user.save((err,userInfo) =>{
     if(err) return res.json({success:false,err})
     return res.status(200).json({
@@ -42,6 +46,41 @@ app.post('/register',(req,res)=>{
 })
 
 
+})
+
+app.post('/login',(req,res)=>{
+
+//요청된 이메일을 데이터베이스에서 있는지 찾는다
+User.findOne({email:req.body.email},(err,user) => {
+
+  if(!user){
+    return res.json({
+
+      loginSuccess:false,
+      message:"제공된 이메일에 해당하는 유저가 없습니다."
+
+    })
+  }
+//요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+  user.comparePassword(req.body.password, (err,isMatch) =>{
+    if(!isMatch)
+      return res.json({loginSuccess:false,message:"비밀번호가 틀렸습니다."
+    })
+    user.generateToken((err,user) => {
+        
+      if(err) return res.status(400).send(err);
+      // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지..
+      res.cookie("x_auth",user.token)
+      .status(200)
+      .json({loginSuccess:true,userId:user._id})
+
+    })
+
+  })
+
+})
+
+//비밀번호 까지 맞다면 토큰을 생성하기
 })
 
 app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`)})
---
+++ models/User.js
@@ -0,0 +1,86 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+const saltRounds =10
+const jwt = require('jsonwebtoken');
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        maxlength:50
+    },
+    email:{
+        type:String,
+        trim:true, //스페이스를 없애줌
+        unique:1
+    },
+    password:{
+        type:String,
+        minlength:5
+    },
+    lastname: {
+        type:String,
+        maxlength:50
+    },
+    role:{
+        type:Number,
+        default:0
+    },
+    image:String,
+    token:{
+        type:String
+
+    },
+    tokenExp:{
+        type:Number
+    }
+})
+
+userSchema.pre('save',function(next){
+    var user = this;
+    //비밀번호를 암호화 시킨다.
+    if(user.isModified('password')){
+
+        bcrypt.genSalt(saltRounds,function(err,salt){
+            if(err) return next(err)
+    
+            bcrypt.hash(user.password,salt,function(err,hash){
+    
+                if(err) return next(err)
+                    user.password = hash
+                    next()
+            })
+        })
+
+    }else{
+        next()
+    }
+
+})
+
+userSchema.methods.comparePassword = function(plainPassword,cb){
+    //plainPassword 1234567 암호화된 비밀번호 ~~
+    bcrypt.compare(plainPassword,this.password,function(err,isMatch){
+        if(err) return cb(err),
+        cb(null,isMatch)
+    })
+}
+
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+//제이슨 웹토큰을 이용해서 토큰 생성하기 
+var token = jwt.sign(user._id.toHexString(),'secretToken')
+// user._id + 'secretToken' = token
+// ->
+// 'secretToken' -> user._id
+    //console.log(user)
+user.token = token
+user.save(function(err,user){
+    if(err) return cb(err)
+    cb(null,user)
+})
+
+}
+
+const User = mongoose.model('User',userSchema)
+module.exports ={User} //다른곳에서도 이 모델을 쓸수있게
+//module.exports =  mongoose.model('User', userSchema)
